2065eb214ad2e53684781259abf97b2c715f755c
Michael Sims <michael@the-sims-family.net>
2023-01-27 18:25:31 -0500

    Added feedforward code

--- src/main/java/frc/robot/RamseteFollower.java
+++ src/main/java/frc/robot/RamseteFollower.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.math.controller.DifferentialDriveFeedforward;
+import edu.wpi.first.math.controller.DifferentialDriveWheelVoltages;
 import edu.wpi.first.math.controller.RamseteController;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
@@ -8,13 +10,21 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.subsystems.drive.DriveCommand;
-import frc.robot.subsystems.drive.DriveCommand.DriveControlMode;
 import frc.robot.subsystems.drive.DriveHAL;
 import frc.robot.subsystems.odometry.OdometryStatus;
 
 public class RamseteFollower {
+
+    private static final double kSLinear = 1.6871;
+    private static final double kVLinear = 3.0149;
+    private static final double kALinear = 5.6755;
+    private static final double kVAngular = 0.05;
+    private static final double kAAngular = 0.05;
+
     private final RamseteController controller;
     private final DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(Units.inchesToMeters(DriveHAL.kTrackWidthInches));
+    private final DifferentialDriveFeedforward feedForward = new DifferentialDriveFeedforward(kVLinear,kALinear,kVAngular,kAAngular,Units.inchesToMeters(DriveHAL.kTrackWidthInches));
+    private final OdometryStatus odometry = OdometryStatus.getInstance();
 
     public RamseteFollower(double b, double zeta) {this(new RamseteController(b, zeta));}
     public RamseteFollower(RamseteController controller)
@@ -43,11 +53,11 @@ public class RamseteFollower {
     public DriveCommand update(double dt)
     {
         lastEvalTime += dt;
-        ChassisSpeeds chassisSpeeds = controller.calculate(OdometryStatus.getInstance().getRobotPose(), trajectory.sample(lastEvalTime - startTime));
-
+        ChassisSpeeds chassisSpeeds = controller.calculate(odometry.getRobotPose(), trajectory.sample(lastEvalTime - startTime));
         DifferentialDriveWheelSpeeds wheelSpeeds = kinematics.toWheelSpeeds(chassisSpeeds);
+        DifferentialDriveWheelVoltages motorVoltages = feedForward.calculate(odometry.getRobotSpeed().left, wheelSpeeds.leftMetersPerSecond, odometry.getRobotSpeed().right, wheelSpeeds.rightMetersPerSecond, dt);
 
-        driveCommand = new DriveCommand(DriveControlMode.VELOCITY_SETPOINT, Units.metersToInches(wheelSpeeds.leftMetersPerSecond), Units.metersToInches(wheelSpeeds.rightMetersPerSecond));
+        driveCommand = new DriveCommand(motorVoltages.left, motorVoltages.right);
         return getDriveCommand();
     }
 
