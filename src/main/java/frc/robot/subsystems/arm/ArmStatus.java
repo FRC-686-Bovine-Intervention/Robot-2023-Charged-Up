bea80c791ede4ca4def8741a2e817ae092fcd54b
Michael Sims <michael@the-sims-family.net>
2023-02-06 19:51:43 -0500

    Added arm pseudo code

--- src/main/java/frc/robot/subsystems/arm/ArmStatus.java
+++ src/main/java/frc/robot/subsystems/arm/ArmStatus.java
@@ -12,12 +12,15 @@ public class ArmStatus extends StatusBase {
     private ArmStatus() {Subsystem = Arm.getInstance();}
 
     public enum ArmState {
-        Defense,    // Arm idling, retracted fully and waiting to grab a piece
-        Grab,       // Intake has a piece and arm is grabbing the piece from the intake
-        Hold,       // Arm has grabbed the piece and is holding it in the defense position
-        Align,      // Robot has entered the community and the turret should align to the alliance wall
-        Extend,     // Driver has selected a node to extend to and arm is extending to it
-        Release     // Driver has decided the piece will score on the node and tells the arm to release the piece
+        Defense,        // Arm is idling, retracted fully and waiting for the intake to do something
+        IdentifyCone,   // Arm is checking if the piece in the intake is a cone
+        IdentifyCube,   // Arm is checking if the piece in the intake is a cube
+        Grab,           // Arm has identified the location of the piece and arm is grabbing the piece from the intake
+        Hold,           // Arm has grabbed the piece and is holding it in the defense position
+        Align,          // Robot has entered the community and the turret should align to the alliance wall
+        Extend,         // Driver has selected a node to extend to and arm is extending to it
+        Adjust,         // Driver and limelight are fudging the position of the turret to align piece on the node
+        Release         // Driver has decided the piece will score on the node and tells the arm to release the piece
     }
 
     private ArmState armState = ArmState.Defense;
