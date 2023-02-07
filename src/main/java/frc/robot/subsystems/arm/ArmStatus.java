71ac82041e67173929b742249266073eb64be363
Michael Sims <michael@the-sims-family.net>
2023-02-06 19:03:42 -0500

    added arm state descriptions

--- src/main/java/frc/robot/subsystems/arm/ArmStatus.java
+++ src/main/java/frc/robot/subsystems/arm/ArmStatus.java
@@ -12,12 +12,12 @@ public class ArmStatus extends StatusBase {
     private ArmStatus() {Subsystem = Arm.getInstance();}
 
     public enum ArmState {
-        Defense,
-        Grab,
-        Hold,
-        Align,
-        Extend,
-        Release
+        Defense,    // Arm idling, retracted fully and waiting to grab a piece
+        Grab,       // Intake has a piece and arm is grabbing the piece from the intake
+        Hold,       // Arm has grabbed the piece and is holding it in the defense position
+        Align,      // Robot has entered the community and the turret should align to the alliance wall
+        Extend,     // Driver has selected a node to extend to and arm is extending to it
+        Release     // Driver has decided the piece will score on the node and tells the arm to release the piece
     }
 
     private ArmState armState = ArmState.Defense;
