217f77c19b976178ebdc5daf8a32b3cf9e49e560
Benjamin Atelsek <kesleta3@gmail.com>
2023-02-07 19:56:06 -0500

    Added turret HAL methods and loop logic

--- src/main/java/frc/robot/subsystems/arm/ArmHAL.java
+++ src/main/java/frc/robot/subsystems/arm/ArmHAL.java
@@ -1,6 +1,13 @@
 package frc.robot.subsystems.arm;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class ArmHAL {
     private static ArmHAL instance;
     public static ArmHAL getInstance(){if(instance == null) instance = new ArmHAL(); return instance;}
+
+    private final 
+
 }
