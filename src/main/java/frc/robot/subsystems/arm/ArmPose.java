b052aefe9142c36adf9e568c7cfbe09ad8cb968a
Bovine Intervention <rich.sims@bovineintervention.org>
2023-02-28 12:26:41 -0500

    Added angles to presets

--- src/main/java/frc/robot/subsystems/arm/ArmPose.java
+++ src/main/java/frc/robot/subsystems/arm/ArmPose.java
@@ -1,10 +1,17 @@
 package frc.robot.subsystems.arm;
 
+import java.util.Optional;
+
+import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.numbers.N2;
 import frc.robot.subsystems.arm.json.ArmPresetsJson;
 
 /** Represents a target position for the arm. */
-public record ArmPose(Translation2d endEffectorPosition) {
+public record ArmPose(
+  Translation2d endEffectorPosition,
+  double shoulderAngleRad,
+  double elbowAngleRad) {
 
   // Preset arm position are read from arm_preset_poses.json
 
@@ -34,8 +41,16 @@ public record ArmPose(Translation2d endEffectorPosition) {
       return pose.endEffectorPosition.getX();
     }
 
-    public double getY() {
+    public double getZ() {
       return pose.endEffectorPosition.getY();
+    }
+
+    public double getShoulderAngleRad() {
+      return pose.shoulderAngleRad;
+    }
+
+    public double getElbowAngleRad() {
+      return pose.elbowAngleRad;
     }
 
     public int getFileIdx() {
@@ -43,31 +58,68 @@ public record ArmPose(Translation2d endEffectorPosition) {
     }
 
 
-    public static void writePresets(ArmPresetsJson jsonPresets) {
+    public static void writePresets(ArmPresetsJson jsonPresets, ArmKinematics kinematics) {
       // write JSON file contents to ArmPose.Presets record
+
+      Translation2d xz;
+      Optional<Vector<N2>> theta;
+      double shoulderAngleRad, elbowAngleRad;
+
+      xz = new Translation2d(jsonPresets.defense().getX(), jsonPresets.defense().getY());
+      theta = kinematics.inverse(xz.getX(), xz.getY());
+      shoulderAngleRad = theta.get().get(0,0);
+      elbowAngleRad = theta.get().get(1,0);
       DEFENSE.fileIdx = jsonPresets.defense().getFileIdx();
-      DEFENSE.pose = new ArmPose(new Translation2d(jsonPresets.defense().getX(), jsonPresets.defense().getY()));
+      DEFENSE.pose = new ArmPose(xz, shoulderAngleRad, elbowAngleRad);
 
+      xz = new Translation2d(jsonPresets.intake().getX(), jsonPresets.intake().getY());
+      theta = kinematics.inverse(xz.getX(), xz.getY());
+      shoulderAngleRad = theta.get().get(0,0);
+      elbowAngleRad = theta.get().get(1,0);
       INTAKE.fileIdx = jsonPresets.intake().getFileIdx();
-      INTAKE.pose = new ArmPose(new Translation2d(jsonPresets.intake().getX(), jsonPresets.intake().getY()));
+      INTAKE.pose = new ArmPose(xz, shoulderAngleRad, elbowAngleRad);
 
+      xz = new Translation2d(jsonPresets.double_substation().getX(), jsonPresets.double_substation().getY());
+      theta = kinematics.inverse(xz.getX(), xz.getY());
+      shoulderAngleRad = theta.get().get(0,0);
+      elbowAngleRad = theta.get().get(1,0);
       DOUBLE_SUBSTATION.fileIdx = jsonPresets.double_substation().getFileIdx();
-      DOUBLE_SUBSTATION.pose = new ArmPose(new Translation2d(jsonPresets.double_substation().getX(),jsonPresets.double_substation().getY()));
+      DOUBLE_SUBSTATION.pose = new ArmPose(xz, shoulderAngleRad, elbowAngleRad);
 
+      xz = new Translation2d(jsonPresets.score_hybrid().getX(), jsonPresets.score_hybrid().getY());
+      theta = kinematics.inverse(xz.getX(), xz.getY());
+      shoulderAngleRad = theta.get().get(0,0);
+      elbowAngleRad = theta.get().get(1,0);
       SCORE_HYBRID.fileIdx = jsonPresets.score_hybrid().getFileIdx();
-      SCORE_HYBRID.pose = new ArmPose(new Translation2d(jsonPresets.score_hybrid().getX(), jsonPresets.score_hybrid().getY()));
+      SCORE_HYBRID.pose = new ArmPose(xz, shoulderAngleRad, elbowAngleRad);
 
+      xz = new Translation2d(jsonPresets.score_mid_cube().getX(), jsonPresets.score_mid_cube().getY());
+      theta = kinematics.inverse(xz.getX(), xz.getY());
+      shoulderAngleRad = theta.get().get(0,0);
+      elbowAngleRad = theta.get().get(1,0);
       SCORE_MID_CUBE.fileIdx = jsonPresets.score_mid_cube().getFileIdx();
-      SCORE_MID_CUBE.pose = new ArmPose(new Translation2d(jsonPresets.score_mid_cube().getX(), jsonPresets.score_mid_cube().getY()));
+      SCORE_MID_CUBE.pose = new ArmPose(xz, shoulderAngleRad, elbowAngleRad);
 
+      xz = new Translation2d(jsonPresets.score_high_cube().getX(), jsonPresets.score_high_cube().getY());
+      theta = kinematics.inverse(xz.getX(), xz.getY());
+      shoulderAngleRad = theta.get().get(0,0);
+      elbowAngleRad = theta.get().get(1,0);
       SCORE_HIGH_CUBE.fileIdx = jsonPresets.score_high_cube().getFileIdx();
-      SCORE_HIGH_CUBE.pose = new ArmPose(new Translation2d(jsonPresets.score_high_cube().getX(), jsonPresets.score_high_cube().getY()));
+      SCORE_HIGH_CUBE.pose = new ArmPose(xz, shoulderAngleRad, elbowAngleRad);
 
+      xz = new Translation2d(jsonPresets.score_mid_cone().getX(), jsonPresets.score_mid_cone().getY());
+      theta = kinematics.inverse(xz.getX(), xz.getY());
+      shoulderAngleRad = theta.get().get(0,0);
+      elbowAngleRad = theta.get().get(1,0);
       SCORE_MID_CONE.fileIdx = jsonPresets.score_mid_cone().getFileIdx();
-      SCORE_MID_CONE.pose = new ArmPose(new Translation2d(jsonPresets.score_mid_cone().getX(), jsonPresets.score_mid_cone().getY()));
+      SCORE_MID_CONE.pose = new ArmPose(xz, shoulderAngleRad, elbowAngleRad);
 
+      xz = new Translation2d(jsonPresets.score_high_cone().getX(), jsonPresets.score_high_cone().getY());
+      theta = kinematics.inverse(xz.getX(), xz.getY());
+      shoulderAngleRad = theta.get().get(0,0);
+      elbowAngleRad = theta.get().get(1,0);
       SCORE_HIGH_CONE.fileIdx = jsonPresets.score_high_cone().getFileIdx();
-      SCORE_HIGH_CONE.pose = new ArmPose(new Translation2d(jsonPresets.score_high_cone().getX(), jsonPresets.score_high_cone().getY()));
+      SCORE_HIGH_CONE.pose = new ArmPose(xz, shoulderAngleRad, elbowAngleRad);
 
     }
   }
