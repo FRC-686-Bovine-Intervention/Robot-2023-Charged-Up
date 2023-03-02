a0ef5e377a41c07622cc721d3b8bab46b2812704
Bovine Intervention <rich.sims@bovineintervention.org>
2023-03-02 14:25:24 -0500

    Paths with wrist mass = 0

--- kairos/matlab/sample.m
+++ kairos/matlab/sample.m
@@ -4,13 +4,13 @@ function [position, velocity, acceleration, jerk] = sample(time, totalTime, poin
     prev1 = floor(time/dt) +1;
     next1 = ceil(time/dt) +1;
     next1(next1==prev1) = next1(next1==prev1)+1;
-    prev2 = prev1-1 +1;
-    next2 = next1+1 +1;
+    prev2 = prev1-1;
+    next2 = next1+1;
 
-    prev1 = clamp(prev1, 0, length(points));
-    next1 = clamp(next1, 0, length(points));
-    prev2 = clamp(prev2, 0, length(points));
-    next2 = clamp(next2, 0, length(points));
+    prev1 = clamp(prev1, 1, length(points));
+    next1 = clamp(next1, 1, length(points));
+    prev2 = clamp(prev2, 1, length(points));
+    next2 = clamp(next2, 1, length(points));
     
     position(1,:) = interp1(linspace(0,totalTime,length(points)), points(1,:), time, 'linear');
     position(2,:) = interp1(linspace(0,totalTime,length(points)), points(2,:), time, 'linear');
