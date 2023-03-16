5733ba58df6ddb8da03786a55a1f4a351c931083
Bovine Intervention <rich.sims@bovineintervention.org>
2023-03-15 20:08:25 -0400

    Updated intake position from handoff measurement

--- kairos/ArmPresets.py
+++ kairos/ArmPresets.py
@@ -43,7 +43,7 @@ class ArmPresetPoses:
 
             "auto_start": {"fileIdx": ArmPresetEnum.AUTO_START, "theta1": -2.381, "theta2": 0.308},
             "defense": {"fileIdx": ArmPresetEnum.DEFENSE, "theta1": -2.408, "theta2": 0.000},
-            "intake": {"fileIdx": ArmPresetEnum.INTAKE, "theta1": -2.266, "theta2": -0.540},
+            "intake": {"fileIdx": ArmPresetEnum.INTAKE, "theta1": -2.048, "theta2": -0.524},
             "double_substation": {"fileIdx": ArmPresetEnum.DOUBLE_SUBSTATION, "theta1": -0.603, "theta2": 0.472},
             "score_hybrid": {"fileIdx": ArmPresetEnum.SCORE_HYBRID, "theta1": -2.000, "theta2": 0.000},
             "score_mid_cube": {"fileIdx": ArmPresetEnum.SCORE_MID_CUBE, "theta1": -1.453, "theta2": 0.400},
