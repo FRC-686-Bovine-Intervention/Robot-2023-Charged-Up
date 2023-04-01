72a3544a520bb0ba32b6d023389277af015c7a8a
Bovine Intervention <rich.sims@bovineintervention.org>
2023-04-01 15:29:25 -0400

    Less downwards during handoff

--- kairos/matlab/design_path_1_9.m
+++ kairos/matlab/design_path_1_9.m
@@ -13,7 +13,7 @@ orig_theta2 = orig_points(2,:);
 
 [corner1_theta1, corner1_theta2, corner2_theta1, corner2_theta2] = get_safe_path_corners();
 
-theta1 = [orig_theta1(1)  -130*pi/180  -160*pi/180  -120*pi/180  orig_theta1(end)];
+theta1 = [orig_theta1(1)  -135*pi/180  -160*pi/180  -120*pi/180  orig_theta1(end)];
 theta2 = [orig_theta2(1)   -45*pi/180   -20*pi/180   +40*pi/180  orig_theta2(end)];
 
 points = [theta1; theta2];
